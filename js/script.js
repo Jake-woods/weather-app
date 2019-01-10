ba36b8f9c63fc19a401b134e6fd7007ebabd3837
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-10 12:53:52 +0000

    Loader behaviour commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-// Function to reduce amount of code needed to select elements
+// Function to reduce amount of typing needed to select elements
 const elementSelector = (ele) => document.querySelector(ele);
 
+// Remove loading screen after 5 seconds (5000)
+const removeLoader = () => {
+   const loadingScreen = elementSelector('.loading-container');
+   loadingScreen.classList.add('loading-container_close');
+}
+setTimeout(removeLoader, 5000);
